612bc3bcbb55c955e9de891614b277c04e230285
christianFedora <christian.harries15@gmail.com>
2026-09-03 08:17:18 +0100

    rm dead code

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,8 @@ use std::env;
 
 use anyhow::{Context, Result};
 
-use serde::{Deserialize, Serialize};
-
 use gocardless::GoCardlessClient;
 
-// STRUCTS
-#[derive(Serialize)]
-struct TokenRequest {
-    secret_id: String,
-    secret_key: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct TokenResponse {
-    access: String,
-    access_expires: i64,
-    refresh: String,
-    refresh_expires: i64,
-}
-
-// // HELPERS
-// async fn authenticate(secret_id: String, secret_key: String) -> Result<TokenResponse> {
-//     let client = reqwest::Client::new();
-//
-//     let body = TokenRequest {
-//         secret_id,
-//         secret_key,
-//     };
-//
-//     let response = client
-//         .post("https://bankaccountdata.gocardless.com/api/v2/token/new/")
-//         .json(&body)
-//         .send()
-//         .await?
-//         .error_for_status()?
-//         .json::<TokenResponse>()
-//         .await?;
-//
-//     Ok(response)
-// }
-
-// MAIN
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
